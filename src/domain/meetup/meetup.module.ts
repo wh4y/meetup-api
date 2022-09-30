417b0020e4514b99eba67b7fd0bf2af39a4db4bc
Anton Veselev <wh4y@mail.ru>
2022-09-30 22:54:07 +0300

    MeetupModule created

---
+++ src/domain/meetup/meetup.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Meetup } from './entity/meetup.entity';
+import { MeetupService } from './service/meetup.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Meetup]),
+  ],
+  providers: [
+    MeetupService,
+  ],
+})
+export class MeetupModule {
+}
