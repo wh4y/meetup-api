708c8c6f9235b2183834561043efcb6028467a76
Anton Veselev <wh4y@mail.ru>
2022-09-30 22:10:24 +0300

    datasource file created

---
+++ typeorm/datasource.js
@@ -0,0 +1,19 @@
+const { config } = require('dotenv');
+const { DataSource } = require('typeorm');
+
+config();
+
+const datasource = new DataSource({
+  type: process.env.DB_DIALECT,
+  host: 'localhost',
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: true,
+  entities: ["./dist/domain/**/*.entity{.ts,.js}"],
+  migrations: ['./typeorm/migrations/*.js'],
+});
+
+exports.datasource = datasource;
