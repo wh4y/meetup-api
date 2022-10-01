d5156a7652ac08a9817ddc21e4c7d4064c295226
Anton Veselev <wh4y@mail.ru>
2022-10-01 17:09:45 +0300

    APP_PORT env var provided & global prefix added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('/api/v1');
+
+  const configService = app.get<ConfigService>(ConfigService);
+  const PORT = configService.get<number>('APP_PORT');
+
+  await app.listen(PORT);
 }
+
 bootstrap();
