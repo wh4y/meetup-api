1aa24dd78460439145540fb7cc7f909333b9ca40
Anton Veselev <wh4y@mail.ru>
2022-10-01 17:13:21 +0300

    meetup controller provided

--- src/domain/meetup/meetup.module.ts
+++ src/domain/meetup/meetup.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Meetup } from './entity/meetup.entity';
 import { MeetupService } from './service/meetup.service';
+import { MeetupController } from '../../app/meetup/controller/meetup.controller';
 
 @Module({
   imports: [
@@ -12,6 +13,9 @@ import { MeetupService } from './service/meetup.service';
   ],
   exports: [
     MeetupService,
+  ],
+  controllers: [
+    MeetupController,
   ],
 })
 export class MeetupModule {
