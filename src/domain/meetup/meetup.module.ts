6dc1058880be60c0705f59e9ac2d8b74ff3cdbd5
Anton Veselev <wh4y@mail.ru>
2022-10-01 18:24:07 +0300

    meetup management service specified

--- src/domain/meetup/meetup.module.ts
+++ src/domain/meetup/meetup.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Meetup } from './entity/meetup.entity';
 import { MeetupService } from './service/meetup.service';
 import { MeetupController } from '../../app/meetup/controller/meetup.controller';
+import { MeetupManagementService } from './service/meetup-management.service';
 
 @Module({
   imports: [
@@ -10,9 +11,11 @@ import { MeetupController } from '../../app/meetup/controller/meetup.controller'
   ],
   providers: [
     MeetupService,
+    MeetupManagementService,
   ],
   exports: [
     MeetupService,
+    MeetupManagementService,
   ],
   controllers: [
     MeetupController,
