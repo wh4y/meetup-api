c8d38db099b8c1037a7dc55176d27129bb022d31
Anton Veselev <wh4y@mail.ru>
2022-10-02 01:51:55 +0300

    pagination extended & findMeetupOptions instead of findManyOptions

--- src/app/meetup/controller/interface/meetup-controller.interface.ts
+++ src/app/meetup/controller/interface/meetup-controller.interface.ts
@@ -1,16 +1,18 @@
 import { Meetup } from '../../../../domain/meetup/entity/meetup.entity';
 import { RegisterMeetupDto } from '../dto/register-meetup.dto';
 import { EditMeetupDto } from '../dto/edit-meetup.dto';
+import { MeetupPageResponse } from '../response/meetup-page.response';
+import { MeetupResponse } from '../response/meetup.response';
 
 
 export interface IMeetupController {
-  getMany(...args: any[]): Promise<Meetup[]>;
+  getMany(...args: any[]): Promise<MeetupPageResponse>;
 
-  register(dto: RegisterMeetupDto): Promise<Meetup>;
+  register(dto: RegisterMeetupDto): Promise<MeetupResponse>;
 
-  getById(id: number): Promise<Meetup>;
+  getById(id: number): Promise<MeetupResponse>;
 
-  cancel(id: number): Promise<Meetup>;
+  cancel(id: number): Promise<MeetupResponse>;
 
-  edit(id: number, dto: EditMeetupDto): Promise<Meetup>;
+  edit(id: number, dto: EditMeetupDto): Promise<MeetupResponse>;
 }
--- src/app/meetup/controller/meetup.controller.ts
+++ src/app/meetup/controller/meetup.controller.ts
@@ -14,11 +14,12 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { IMeetupController } from './interface/meetup-controller.interface';
-import { Meetup } from '../../../domain/meetup/entity/meetup.entity';
 import { MeetupService } from '../../../domain/meetup/service/meetup.service';
 import { RegisterMeetupDto } from './dto/register-meetup.dto';
 import { EditMeetupDto } from './dto/edit-meetup.dto';
 import { FindMeetupDto } from './dto/find-meetup.dto';
+import { MeetupResponse } from './response/meetup.response';
+import { MeetupPageResponse } from './response/meetup-page.response';
 
 
 @UsePipes(new ValidationPipe({ transform: true }))
@@ -30,33 +31,36 @@ export class MeetupController implements IMeetupController {
   }
 
   @Get('/')
-  async getMany(
-    @Query() dto: FindMeetupDto,
-  ): Promise<Meetup[]> {
-    const findMeetupOptions = { ...dto };
-    Reflect.deleteProperty(findMeetupOptions, 'page');
-    Reflect.deleteProperty(findMeetupOptions, 'count');
+  async getMany(@Query() dto: FindMeetupDto): Promise<MeetupPageResponse> {
+    const findManyOptions = { ...dto };
+    Reflect.deleteProperty(findManyOptions, 'page');
+    Reflect.deleteProperty(findManyOptions, 'count');
 
-    return await this.meetupService.findMany(findMeetupOptions, dto.page, dto.count);
+    const pageNumber = dto.page ? dto.page : 1;
+    const count = dto.count ? dto.count : null;
+
+    const meetups = await this.meetupService.findMany(findManyOptions, pageNumber, count);
+    const totalCount = await this.meetupService.getTotalCount(findManyOptions);
+
+    return { meetups, pageNumber, totalCount };
   }
 
   @HttpCode(HttpStatus.CREATED)
   @Post('/register')
-  async register(@Body() dto: RegisterMeetupDto): Promise<Meetup> {
-    const findMeetupOptions = { ...dto };
-    const meetup = await this.meetupService.registerMeetup(findMeetupOptions);
+  async register(@Body() dto: RegisterMeetupDto): Promise<MeetupResponse> {
+    const meetup = await this.meetupService.registerMeetup({ ...dto });
 
     return meetup;
   }
 
   @Get('/:id')
-  async getById(@Param('id', new ParseIntPipe()) id: number): Promise<Meetup> {
+  async getById(@Param('id', new ParseIntPipe()) id: number): Promise<MeetupResponse> {
     return await this.meetupService.findById(id);
   }
 
   @HttpCode(HttpStatus.OK)
   @Delete('/cancel/:id')
-  async cancel(@Param('id', new ParseIntPipe()) id: number): Promise<Meetup> {
+  async cancel(@Param('id', new ParseIntPipe()) id: number): Promise<MeetupResponse> {
     const meetup = await this.meetupService.cancelMeetup(id);
 
     return meetup;
@@ -67,7 +71,7 @@ export class MeetupController implements IMeetupController {
   async edit(
     @Param('id', new ParseIntPipe()) id: number,
     @Body() dto: EditMeetupDto,
-  ): Promise<Meetup> {
+  ): Promise<MeetupResponse> {
     const meetup = await this.meetupService.editMeetup(id, { ...dto });
 
     return meetup;
