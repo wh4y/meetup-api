36ab073c6f25d58dcd38ae458d2351b51d88bd3f
Anton Veselev <wh4y@mail.ru>
2022-10-01 19:37:24 +0300

    env validation implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,21 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './infra/database/database.module';
 import { MeetupModule } from './domain/meetup/meetup.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        APP_PORT: Joi.number().port().required(),
+        DB_DIALECT: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().port().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     DatabaseModule.registerAsync(),
     MeetupModule,
