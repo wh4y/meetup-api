cdc76654be1c4f295831e53744b261b33328c0cd
Anton Veselev <wh4y@mail.ru>
2022-10-01 22:31:31 +0300

    FindMeetupDto created

---
+++ src/app/meetup/controller/dto/find-meetup.dto.ts
@@ -0,0 +1,12 @@
+import { FindMeetupOptions } from '../../../../domain/meetup/service/options/find-meetup.options';
+import { Transform } from 'class-transformer';
+
+export class FindMeetupDto implements FindMeetupOptions {
+  @Transform(({ value }) => Number(value))
+  public id?: number;
+  public title?: string;
+  public description?: string;
+  public datetime?: Date;
+  public address?: string;
+  public tags?: string [];
+}
