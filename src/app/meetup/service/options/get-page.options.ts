0c4c30ea5c85f08837bd4b8e584175e8cd6eb2c8
Anton Veselev <wh4y@mail.ru>
2022-10-02 02:42:09 +0300

    GetPageOptions created

---
+++ src/app/meetup/service/options/get-page.options.ts
@@ -0,0 +1,10 @@
+export interface GetPageOptions {
+  id?: number;
+  title?: string;
+  description?: string;
+  datetime?: Date;
+  address?: string;
+  tags?: string [];
+  page?: number;
+  count?: number;
+}
