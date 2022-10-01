b8146c433aaf01973887546e9cd20f1d5671a448
Anton Veselev <wh4y@mail.ru>
2022-10-01 20:37:54 +0300

    dto validation implemented

--- src/app/meetup/controller/dto/edit-meetup.dto.ts
+++ src/app/meetup/controller/dto/edit-meetup.dto.ts
@@ -1,9 +1,32 @@
 import { UpdateMeetupOptions } from '../../../../domain/meetup/service/options/update-meetup.options';
+import { ArrayNotEmpty, IsArray, IsDateString, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class EditMeetupDto implements UpdateMeetupOptions {
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   public title?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   public description?: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  @IsOptional()
   public datetime?: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   public address?: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsNotEmpty({ each: true })
+  @ArrayNotEmpty()
+  @IsOptional()
   public tags?: string [];
 }
--- src/app/meetup/controller/dto/register-meetup.dto.ts
+++ src/app/meetup/controller/dto/register-meetup.dto.ts
@@ -1,9 +1,26 @@
 import { CreateMeetupOptions } from '../../../../domain/meetup/entity/options/create-meetup.options';
+import { ArrayNotEmpty, IsArray, IsDateString, IsNotEmpty, IsString } from 'class-validator';
 
 export class RegisterMeetupDto implements CreateMeetupOptions {
+  @IsDateString()
+  @IsNotEmpty()
   public datetime: Date;
+
+  @IsString()
+  @IsNotEmpty()
   public address: string;
+
+  @IsString()
+  @IsNotEmpty()
   public description: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsNotEmpty({ each: true })
+  @ArrayNotEmpty()
   public tags: string[];
+
+  @IsString()
+  @IsNotEmpty()
   public title: string;
 }
