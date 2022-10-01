3151a9579fca0426b74f3e19c0b54f5ac6f38ffc
Anton Veselev <wh4y@mail.ru>
2022-10-01 23:45:44 +0300

    FindMeetupDto validation implemented

--- src/app/meetup/controller/dto/find-meetup.dto.ts
+++ src/app/meetup/controller/dto/find-meetup.dto.ts
@@ -1,16 +1,51 @@
 import { FindMeetupOptions } from '../../../../domain/meetup/service/options/find-meetup.options';
 import { Transform } from 'class-transformer';
+import { ArrayNotEmpty, IsArray, IsDate, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class FindMeetupDto implements FindMeetupOptions {
   @Transform(({ value }) => Number(value))
+  @IsNumber()
+  @IsNotEmpty()
+  @IsOptional()
   public id?: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   public title?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   public description?: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  @IsOptional()
+  @Transform(({ value }) => new Date(value))
   public datetime?: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   public address?: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsNotEmpty({ each: true })
+  @ArrayNotEmpty()
+  @IsOptional()
   public tags?: string [];
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsOptional()
   @Transform(({ value }) => Number(value))
   public page?: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsOptional()
   @Transform(({ value }) => Number(value))
   public count?: number;
 }
