534f6f97e449960de7bbdebf76704f980344a823
Anton Veselev <wh4y@mail.ru>
2022-10-02 20:52:07 +0300

    MeetupMapper implements IMeetupMapper interface

--- src/meetup/controller/mapper/meetup/meetup.mapper.ts
+++ src/meetup/controller/mapper/meetup/meetup.mapper.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { Meetup } from 'src/meetup/entity/meetup/meetup.entity';
 import { MeetupPageResponse } from '../../response/meetup-page.response';
+import { IMeetupMapper } from './interface/meetup-mapper.interface';
 
 @Injectable()
-export class MeetupMapper {
+export class MeetupMapper implements IMeetupMapper {
   public async mapMeetupsToPage(meetups: Meetup[], totalCount: number, pageNumber: number): Promise<MeetupPageResponse> {
     const page = { meetups, totalCount, pageNumber };
     Reflect.setPrototypeOf(page, MeetupPageResponse.prototype);
