b785d5632c5459855c5afcc16d87f0d8ee06f44b
Anton Veselev <wh4y@mail.ru>
2022-10-02 18:34:01 +0300

    set page = 1 if it's not defined

--- src/meetup/controller/pipe/pagination-dto.pipe.ts
+++ src/meetup/controller/pipe/pagination-dto.pipe.ts
@@ -5,7 +5,7 @@ import { MeetupPaginationDto } from '../dto/meetup-pagination.dto';
 export class PaginationDtoPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata): MeetupPaginationDto {
     return {
-      page: value.page,
+      page: value.page ? value.page : 1,
       count: value.count,
     };
   }
