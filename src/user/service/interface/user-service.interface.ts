b8aa39cd58bc77c2481fcde2d112b0bbff4d0a36
Anton Veselev <wh4y@mail.ru>
2022-10-03 22:31:04 +0300

    user service interface created

---
+++ src/user/service/interface/user-service.interface.ts
@@ -0,0 +1,15 @@
+import { CreateUserOptions } from '../../entity/options/create-user.options';
+import { User } from '../../entity/user.entity';
+import { UpdateUserOptions } from '../options/update-user.options';
+
+export interface IUserService {
+  findById(id: number): Promise<User>;
+
+  findMany(options: any): Promise<User[]>;
+
+  create(options: CreateUserOptions): Promise<User>;
+
+  update(id: number, options: UpdateUserOptions): Promise<User>;
+
+  delete(id: number): Promise<User>;
+}
