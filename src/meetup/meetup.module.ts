69b0acdfc210183374f9c5f2f9fabfc5095dfe53
Anton Veselev <wh4y@mail.ru>
2022-10-04 20:11:16 +0300

    auth module imported in meetup module

--- src/meetup/meetup.module.ts
+++ src/meetup/meetup.module.ts
@@ -4,10 +4,12 @@ import { Meetup } from './entity/meetup/meetup.entity';
 import { MeetupService } from './service/meetup.service';
 import { MeetupController } from './controller/meetup.controller';
 import { MeetupMapper } from './controller/mapper/meetup/meetup.mapper';
+import { AuthModule } from './module/auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Meetup]),
+    AuthModule,
   ],
   providers: [
     MeetupMapper,
