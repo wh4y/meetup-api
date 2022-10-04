fd875d97635b41a1306c5b02eefda777fc0e0476
Anton Veselev <wh4y@mail.ru>
2022-10-04 14:50:03 +0300

    user controller imported in user module

--- src/meetup/module/auth/module/user/user.module.ts
+++ src/meetup/module/auth/module/user/user.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { UserService } from './service/user.service';
+import { UserController } from './controller/user.controller';
 
 @Module({
   imports: [
@@ -12,6 +13,9 @@ import { UserService } from './service/user.service';
   ],
   exports: [
     UserService,
+  ],
+  controllers: [
+    UserController,
   ],
 })
 export class UserModule {
