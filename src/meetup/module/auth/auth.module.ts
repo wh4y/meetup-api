0b990d26e0dba273096bfaafea80a02e0dbce379
Anton Veselev <wh4y@mail.ru>
2022-10-04 23:34:07 +0300

    providers & controller imported

--- src/meetup/module/auth/auth.module.ts
+++ src/meetup/module/auth/auth.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './module/user/user.module';
+import { AuthService } from './service/auth.service';
+import { LocalStrategy } from './controller/strategy/local.strategy';
+import { AuthController } from './controller/auth.controller';
 
 @Module({
   imports: [
@@ -7,6 +10,13 @@ import { UserModule } from './module/user/user.module';
   ],
   exports: [
     UserModule,
+  ],
+  providers: [
+    AuthService,
+    LocalStrategy,
+  ],
+  controllers: [
+    AuthController,
   ],
 })
 export class AuthModule {
