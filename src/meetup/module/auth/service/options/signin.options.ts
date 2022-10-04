40c754847f785c836f099470e3133a171c65c4b2
Anton Veselev <wh4y@mail.ru>
2022-10-04 20:42:13 +0300

    SignIn & SignUp options created

---
+++ src/meetup/module/auth/service/options/signin.options.ts
@@ -0,0 +1,4 @@
+export interface SignInOptions {
+  email: string;
+  password: string;
+}
---
+++ src/meetup/module/auth/service/options/signup.options.ts
@@ -0,0 +1,6 @@
+export interface SignUpOptions {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}
