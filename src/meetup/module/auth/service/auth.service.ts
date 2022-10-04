baef9c8f9e6e03f503b0e3e7a844589b78f6be86
Anton Veselev <wh4y@mail.ru>
2022-10-04 20:43:54 +0300

    auth service typed according to interface

--- src/meetup/module/auth/service/auth.service.ts
+++ src/meetup/module/auth/service/auth.service.ts
@@ -1,13 +1,16 @@
 import { IAuthService } from './interface/auth-service.interface';
 import { Injectable } from '@nestjs/common';
+import { User } from '../module/user/entity/user.entity';
+import { SignInOptions } from './options/signin.options';
+import { SignUpOptions } from './options/signup.options';
 
 @Injectable()
 export class AuthService implements IAuthService {
-  signIn(options: unknown): Promise<unknown> {
+  signIn(options: SignInOptions): Promise<User> {
     return Promise.resolve(undefined);
   }
 
-  signUp(options: unknown): Promise<unknown> {
+  signUp(options: SignUpOptions): Promise<User> {
     return Promise.resolve(undefined);
   }
 }
