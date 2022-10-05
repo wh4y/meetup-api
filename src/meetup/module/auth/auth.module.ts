ec4017a5951867a870bb0fe15f40313b468e2823
Anton Veselev <wh4y@mail.ru>
2022-10-05 21:47:22 +0300

    JWT strategy & module registered

--- src/meetup/module/auth/auth.module.ts
+++ src/meetup/module/auth/auth.module.ts
@@ -3,10 +3,21 @@ import { UserModule } from './module/user/user.module';
 import { AuthService } from './service/auth.service';
 import { LocalStrategy } from './controller/strategy/local.strategy';
 import { AuthController } from './controller/auth.controller';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './controller/strategy/jwt.strategy';
 
 @Module({
   imports: [
     UserModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService): Promise<JwtModuleOptions> => {
+        return {
+          secret: configService.get<string>('JWT_ACCESS_SECRET'),
+        };
+      },
+      inject: [ConfigService],
+    }),
   ],
   exports: [
     UserModule,
@@ -14,6 +25,7 @@ import { AuthController } from './controller/auth.controller';
   providers: [
     AuthService,
     LocalStrategy,
+    JwtStrategy,
   ],
   controllers: [
     AuthController,
