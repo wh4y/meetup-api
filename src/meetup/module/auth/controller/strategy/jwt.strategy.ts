97bd98c31f7d32319d801d4a2bbca17787edd14e
Anton Veselev <wh4y@mail.ru>
2022-10-05 21:45:11 +0300

    JWT strategy registered

---
+++ src/meetup/module/auth/controller/strategy/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { User } from '../../module/user/entity/user.entity';
+import { JwtPayload } from '../interface/jwt-payload.interface';
+import { AuthService } from '../../service/auth.service';
+import extractJwtFromCookies from '../../../../../shared/module/Jwt/util/extractJwtFromCookies';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: extractJwtFromCookies('accessToken'),
+      secretOrKey: configService.get<string>('JWT_ACCESS_SECRET'),
+      ignoreExpiration: false,
+      passReqToCallback: false,
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+
+    return await this.authService.verifyJWTPayload(payload);
+  }
+}
