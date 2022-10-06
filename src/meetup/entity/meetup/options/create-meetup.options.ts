076a4b66ec3b9bc9d7bd7a6d64f092bf591b1874
Anton Veselev <wh4y@mail.ru>
2022-10-06 17:50:36 +0300

    create meetup options no longer describes guests option

--- src/meetup/entity/meetup/options/create-meetup.options.ts
+++ src/meetup/entity/meetup/options/create-meetup.options.ts
@@ -1,10 +1,7 @@
-import { User } from '../../../module/auth/module/user/entity/user.entity';
-
 export interface CreateMeetupOptions {
   title: string;
   description: string;
   datetime: Date;
   address: string;
   tags: string [];
-  guests?: User[];
 }
