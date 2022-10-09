71fb6ee30975c4c98197b138f99e6f00e4dfdde4
Anton Veselev <wh4y@mail.ru>
2022-10-09 13:11:01 +0300

    JwtModule no longer needs register options

--- src/meetup/module/auth/auth.module.ts
+++ src/meetup/module/auth/auth.module.ts
@@ -3,21 +3,13 @@ import { UserModule } from './module/user/user.module';
 import { AuthService } from './service/auth.service';
 import { LocalStrategy } from './controller/strategy/local.strategy';
 import { AuthController } from './controller/auth.controller';
-import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AccessJwtStrategy } from './controller/strategy/access-jwt.strategy';
 
 @Module({
   imports: [
     UserModule,
-    JwtModule.registerAsync({
-      useFactory: async (configService: ConfigService): Promise<JwtModuleOptions> => {
-        return {
-          secret: configService.get<string>('JWT_ACCESS_SECRET'),
-        };
-      },
-      inject: [ConfigService],
-    }),
+    JwtModule,
   ],
   exports: [
     UserModule,
