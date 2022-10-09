6828265fe4dc91e1b6e19050bbcb57d0cce6befd
Anton Veselev <wh4y@mail.ru>
2022-10-09 14:29:39 +0300

    cookie parser used in app

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1');
+
+  app.use(cookieParser());
 
   const config = new DocumentBuilder()
     .setTitle('Meetup API')
