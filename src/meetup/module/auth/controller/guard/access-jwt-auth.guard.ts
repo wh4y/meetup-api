5d0a26f57ad9d9d68a0372695b139772ce2598b7
Anton Veselev <wh4y@mail.ru>
2022-10-09 22:36:57 +0300

    jwt auth guards created

--- src/meetup/module/auth/controller/guard/access-jwt-auth.guard.ts
+++ src/meetup/module/auth/controller/guard/access-jwt-auth.guard.ts
@@ -1,6 +1,7 @@
 import { AuthGuard } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { ACCESS_JWT_STRATEGY } from '../strategy/access-jwt.strategy';
 
 @Injectable()
-export class AccessJwtAuthGuard extends AuthGuard('access-jwt') {
+export class AccessJwtAuthGuard extends AuthGuard(ACCESS_JWT_STRATEGY) {
 }
--- src/meetup/module/auth/controller/guard/refresh-jwt-auth.guard.ts
+++ src/meetup/module/auth/controller/guard/refresh-jwt-auth.guard.ts
@@ -1,6 +1,7 @@
 import { AuthGuard } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { REFRESH_JWT_STRATEGY } from '../strategy/refresh-jwt.strategy';
 
 @Injectable()
-export class RefreshJwtAuthGuard extends AuthGuard('refresh-jwt') {
+export class RefreshJwtAuthGuard extends AuthGuard(REFRESH_JWT_STRATEGY) {
 }
