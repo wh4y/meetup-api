7281074467dc5353bcd1616fe28525a60b521567
Anton Veselev <wh4y@mail.ru>
2022-10-09 14:30:18 +0300

    logic refactored with cookie parser

--- src/shared/module/auth/Jwt/util/extractJwtFromCookies.ts
+++ src/shared/module/auth/Jwt/util/extractJwtFromCookies.ts
@@ -1,19 +1,8 @@
 import { Request } from 'express';
 import { JwtFromRequestFunction } from 'passport-jwt';
-import { UnauthorizedException } from '@nestjs/common';
 
 const extractJwtFromCookies = (cookie: string) => ((req: Request) => {
-  const cookies = req.headers.cookie;
-  if (!cookies) throw new UnauthorizedException();
-
-  const token = cookies
-    .split(' ')
-    .filter((item) => item.includes(cookie))
-    .pop()
-    .replace(new RegExp(`${cookie}=`), '')
-    .replace(/;/, '');
-
-  return token;
+  return req.cookies[cookie];
 }) as JwtFromRequestFunction;
 
 export default extractJwtFromCookies;
