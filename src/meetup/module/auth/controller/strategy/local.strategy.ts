235b660eb566102e6bc5fdeb0c64762f56dda186
Anton Veselev <wh4y@mail.ru>
2022-10-11 01:51:58 +0300

    dto validation within strategy implemented

--- src/meetup/module/auth/controller/strategy/local.strategy.ts
+++ src/meetup/module/auth/controller/strategy/local.strategy.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../../service/auth/auth.service';
 import { Request } from 'express';
 import { User } from '../../module/user/entity/user.entity';
 import { SignInDto } from '../dto/signin.dto';
+import { plainToInstance } from 'class-transformer';
+import { validate as validateDto } from 'class-validator';
+import { throttle } from 'rxjs';
 
 export const LOCAL_STRATEGY = 'LOCAL_STRATEGY';
 
@@ -19,7 +22,14 @@ export class LocalStrategy extends PassportStrategy(Strategy, LOCAL_STRATEGY) {
   }
 
   async validate(req: Request): Promise<User> {
-    const dto: SignInDto = req.body;
+    const dto: SignInDto = plainToInstance(SignInDto, req.body);
+    await validateDto(dto).then((errors) => {
+      let messages = [];
+      errors.forEach(error => messages.push(...Object.values(error.constraints)));
+
+      throw new BadRequestException(messages);
+    });
+
     return await this.authService.signIn(dto);
   }
 }
